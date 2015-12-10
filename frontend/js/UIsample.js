a6ffcecd5ea0178aeb1a411afbc66ff2118d23b3
akellaumesh <umesh.akella@gmail.com>
2015-12-10 11:44:09 +0100

    Grid edited

--- frontend/js/UIsample.js
+++ frontend/js/UIsample.js
@@ -1,20 +1,15 @@
-var app = angular.module('myApp', ['ngGrid']);
+var app = angular.module('myApp', ['ui.grid','ui.grid.selection', 'ui.grid.cellNav']);
 app.controller('MyCtrl', function($scope, $http) {
  
-  $scope.gridOptions = {
-    data: 'Employees',
-    columnDefs: [{field:'eid', displayName:'Employee ID'}, {field:'ename', displayName:'Employee Name'}, {field:'email', displayName:'E-mail'}, {field:'age', displayName:'Age'}, {field:'sex', displayName:'Sex'}],
-    enableSorting:true,	
-    showGroupPanel: true,
-    enableGridMenu:true,
-    onRegisterApi: function (gridApi) {
-            $scope.gridApi = gridApi;
-    gridApi.rowEdit.on.saveRow($scope, $scope.saveRow)
-}  
-};
-
+  $scope.gridOptions = {};
       $http.get('/api/user').then(function(response) {
          $scope.users= response.data;
-      });      
+	$scope.gridOptions.data = response.data;
+
+      });
+
+      
 
 });
+
+
